d51c932b48308ccd5a6bed8dfe5c79852149734a
Andrea Botello <andrea.botellor@misena.edu.co>
2021-12-01 19:33:46 -0500

    updated

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,11 +1,25 @@
 <template lang="pug">
 .curso-main-container.introduccion
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5.bloque-fondo-intro
   
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span
+          i.fas.fa-info
+      h1 Introducción
     
-    
+    figure.mb-4
+      img(src='@/assets/curso/intro-banner.png', alt='Introducción')
 
+    p.mb-4(data-aos="fade") El ser humano cuenta con un sistema visual que reúne grandes características, porque a través de él se pueden diferenciar, detectar, analizar y almacenar imágenes con gran potencia y un sin fin de formas y colores. Es un mecanismo perfecto de cómo el cerebro procesa las imágenes y la forma como cambia la información visual para conseguir mejores resultados o para aislar una particularidad determinada de una imagen. El impacto de este método es gigantesco, tanto así que se relaciona con los campos de las telecomunicaciones, la medicina, el entrenamiento y el control de procesos industriales. A continuación, se detallan elementos complementarios de la introducción al componente.
+
+    .row.justify-content-center.mb-4(data-aos="fade-down") 
+      .col-md-10
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
+  
 
     
 </template>
